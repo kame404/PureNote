fa730f0509ada8286e610201d8312c8db4aef4da
kame404 <54836559+kame404@users.noreply.github.com>
2020-02-12 00:22:18 +0900

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PureNote
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
+
+            RichTextBox.ContextMenu = null;
+
+            Point startPosition = new Point();
+            this.PreviewMouseRightButtonDown += (sender, e) =>
+            {
+                startPosition = e.GetPosition(this);
+            };
+
+            this.PreviewMouseMove += (sender, e) =>
+            {
+                if (e.RightButton == MouseButtonState.Pressed)
+                {
+                    Point endPosition = e.GetPosition(this);
+                    Vector vector = endPosition - startPosition;
+                    this.Left += vector.X;
+                    this.Top += vector.Y;
+                }
+            };
+        }
+
+        private void HandleEsc(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                WindowDialog();
+        }
+        private void WindowDialog()
+        {
+            MessageBoxResult msbr = MessageBox.Show("ウインドウを閉じますか？", "PureNote", MessageBoxButton.OKCancel);
+
+            if (msbr == MessageBoxResult.OK)
+            {
+                Close();
+            }
+
+        }
+    }
+}
